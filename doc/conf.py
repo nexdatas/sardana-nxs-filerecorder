4d0a93db11ce1c4fee3f7677853089cb5ef10c86
Jan Kotanski <jankotan@gmail.com>
2019-05-29 08:25:43 +0200

    update manpages

--- doc/conf.py
+++ doc/conf.py
@@ -420,7 +420,8 @@ epub_exclude_files = ['search.html']
 autoclass_content = 'both'
 
 intersphinx_mapping = {
-    'http://www.taurus-scada.org/en/3.7.3/': None,
+    # 'http://www.taurus-scada.org/en/3.7.3/': None,
+    'http://www.taurus-scada.org': None,
     'http://www.sardana-controls.org': None,
     'https://nexdatas.github.io/configserver/': None,
     'https://nexdatas.github.io/writer/': None,
