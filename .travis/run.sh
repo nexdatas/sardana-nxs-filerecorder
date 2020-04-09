b46a6fb664fba665b3cb2fc58a713fa55479f1a2
Jan Kotanski <jankotan@gmail.com>
2020-04-09 09:48:51 +0200

    update tests for focal

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ $1 == "2" ]]; then
-    echo "run python-nxselector"
+    echo "run python-sardana-nxsrecorder"
     docker exec -it ndts python test
 else
-    echo "run python3-nxselector"
+    echo "run python3-sardana-nxsrecorder"
     docker exec -it ndts python3 test
 fi
 if [ "$?" -ne "0" ]
