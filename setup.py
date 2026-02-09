6a8fd3faffa8bb038e1a36f042a96efcb09c7fc4
Jan Kotanski <jankotan@gmail.com>
2026-02-09 13:54:12 +0100

    add python 3.13

--- setup.py
+++ setup.py
@@ -90,6 +90,7 @@ SETUPDATA = dict(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
     install_requires=install_requires,
     zip_safe=False,
